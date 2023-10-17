581523ac6c349eddacff66625cd39dac45accd96
pascoall <79522993+pascoall@users.noreply.github.com>
2023-10-17 12:28:25 -0300

    Add files via upload

---
+++ somando_em_python.py
@@ -0,0 +1,8 @@
+a = float(input('Digite um valor: '))
+b = float(input('Digite um valor: '))
+c = float(input('Digite um valor: '))
+d = float(input('Digite um valor: '))
+e = float(input('Digite um valor: '))
+
+soma = a + b+ c + d + e
+print(soma)
